452a5b8ca316e28d6ea9dd0faddad0b996c51285
manuel <miglesiassarrai@gmail.com>
2017-04-26 14:43:13 +0200

    paquetes

--- python_initial.sh
+++ python_initial.sh
@@ -18,25 +18,34 @@ if [ ! -f /solutions/app/java/bin/java ]; then
     wget -P /solutions/app/rpm https://github.com/nfqSolutionsDocker/python/raw/3.4-oracle/package/3_cx_5.1.2-11g.rpm
 fi
 
+ls -l ${RPM_PATH}/*.rpm > /dev/null 2>&1
+if [ "$?" = "0" ]; then
 echo Instalando ficheros RPM
 for rpm_file in ${RPM_PATH}/*.rpm; do
 	rpm -Uvh ${rpm_file}
 done
+fi
 
 echo Instalando paquetes oficiales en PIP
 for pip_package in $(echo ${PIP_PACKAGES} | sed 's/,/\n/g'); do
 	pip3.4 install ${pip_package}
 done
 
-echo Instalando paquetes locales en PIP (.zip)
+ls -l ${PIP_PATH}/*.zip > /dev/null 2>&1
+if [ "$?" = "0" ]; then
+echo Instalando paquetes locales en PIP .zip
 for pip_zip in ${PIP_PATH}/*.zip; do
 	pip3.4 install ${pip_zip}
 done
+fi
 
-echo Instalando paquetes locales en PIP (.tar.gz)
+ls -l ${PIP_PATH}/*.tar.gz > /dev/null 2>&1
+if [ "$?" = "0" ]; then
+echo Instalando paquetes locales en PIP .tar.gz
 for pip_targz in ${PIP_PATH}/*.tar.gz; do
 	pip3.4 install ${pip_targz}
 done
+fi
 
 echo Ejecutando ficheros python
 for python_file in $(echo ${PYTHON_FILES} | sed 's/,/\n/g'); do
