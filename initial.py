a961b171753063f23270a4f5824b69b2fe7a9805
raul_nunez <raul.nunez@nfq.es>
2017-06-14 13:46:37 +0200

    Añado la posibilidad de ejecutar servicios que se hayan instalado durante el arranque

--- initial.py
+++ initial.py
@@ -8,29 +8,34 @@ RPM_PATH = os.getenv('RPM_PATH')
 PIP_PACKAGES = os.getenv('PIP_PACKAGES')
 PIP_PATH = os.getenv('PIP_PATH')
 PYTHON_FILES = os.getenv('PYTHON_FILES')
+SERVICES = os.getenv('SERVICES')
 
 for file in os.listdir(RPM_PATH):
     if file.endswith('.rpm'):
-        print('Instalando ficheros RPM')
+        print('Instalando ficheros RPM: {}'.format(file))
         os.system('rpm -Uvh {}/{}'.format(RPM_PATH,file))
 
 for package in PIP_PACKAGES.split(','):
-    print('Instalando paquetes oficiales en PIP')
+    print('Instalando paquetes oficiales en PIP: {}'.format(package))
     os.system('pip3.6 install {}'.format(package))
 
 for file in os.listdir(PIP_PATH):
     if file.endswith('.zip'):
-        print('Instalando paquetes locales en PIP .zip')
+        print('Instalando paquetes locales en PIP .zip: {}'.format(file))
         os.system('pip3.6 install {}/{}'.format(PIP_PATH,file))
 
 for file in os.listdir(PIP_PATH):
     if file.endswith('.tar.gz'):
-        print('Instalando paquetes locales en PIP .tar.gz')
+        print('Instalando paquetes locales en PIP .tar.gz: {}'.format(file))
         os.system('pip3.6 install {}/{}'.format(PIP_PATH,file))
 
 for file in PYTHON_FILES.split(','):
-    print('Ejecutando ficheros python')
+    print('Ejecutando ficheros python: {}'.format(file))
     os.system('python3.6 {} &'.format(file))
+
+for file in SERVICES.split(','):
+    print('Ejecutando service: {}'.format(file))
+    os.system('{} &'.format(file))
 
 print('Ejecutando bash ...')
 os.system('/bin/bash')
