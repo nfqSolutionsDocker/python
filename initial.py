927e94cb4c972e03c688c5a4b78483b410f85f7d
raul <raul.nunez@nfq.es>
2017-08-03 15:55:53 +0200

    Check environment variables

--- initial.py
+++ initial.py
@@ -10,18 +10,21 @@ PIP_PATH = os.getenv('PIP_PATH')
 PYTHON_FILES = os.getenv('PYTHON_FILES')
 SERVICES = os.getenv('SERVICES')
 
-if RPM_PATH is not None:
+# FALSE if RPM_PATH is '' or None
+if bool(RPM_PATH):
     for file in os.listdir(RPM_PATH):
         if file.endswith('.rpm'):
             print('Instalando ficheros RPM: {}'.format(file))
             os.system('rpm -Uvh {}/{}'.format(RPM_PATH,file))
 
-if PIP_PACKAGES is not None:
+# FALSE if PIP_PACKAGES is '' or None
+if bool(PIP_PACKAGES):
     for package in PIP_PACKAGES.split(','):
         print('Instalando paquetes oficiales en PIP: {}'.format(package))
         os.system('pip3.6 install {}'.format(package))
 
-if PIP_PATH is not None:
+# FALSE if PIP_PATH is '' or None
+if bool(PIP_PATH):
     for file in os.listdir(PIP_PATH):
         if file.endswith('.zip'):
             print('Instalando paquetes locales en PIP .zip: {}'.format(file))
@@ -31,12 +34,14 @@ if PIP_PATH is not None:
             print('Instalando paquetes locales en PIP .tar.gz: {}'.format(file))
             os.system('pip3.6 install {}/{}'.format(PIP_PATH, file))
 
-if PYTHON_FILES is not None:
+# FALSE if PYTHON_FILES is '' or None
+if bool(PYTHON_FILES):
     for file in PYTHON_FILES.split(','):
         print('Ejecutando ficheros python: {}'.format(file))
         os.system('python3.6 {} &'.format(file))
 
-if SERVICES is not None:
+# FALSE if SERVICES is '' or None
+if bool(SERVICES):
     for file in SERVICES.split(','):
         print('Ejecutando service: {}'.format(file))
         os.system('{} &'.format(file))
