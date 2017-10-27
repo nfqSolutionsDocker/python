df998f2407bc9b9859f7fcd66202f36300624737
manuel <manuel.iglesias@nfq.es>
2017-10-27 14:51:28 +0200

    Palm fucionando con base miniconda

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,10 @@
-FROM nfqsolutions/centos:7
+FROM conda/miniconda3
 
 MAINTAINER solutions@nfq.com
 
-# Instalacion previa
-RUN sudo yum install -y epel-release
-RUN sudo yum install -y gcc \
-	wget \
-	libaio \
-    blas_devel \
-    fftw \
-    fftw-devel \
-    atlas-devel \
-    atlas \
-    atlas-sse3 \
-    make \
-    mysql \
-    mysql-devel \
-    lapack \
-    lapack-devel \
-    lapack-static \
-    zlib \
-    zlib-devel \
-    sqlite-devel
 
-# Instalacion python 3.6
-RUN wget -P /usr/src/ "https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz" && \
-	tar -xzf /usr/src/Python-3.6.1.tgz -C /usr/src/ && \
-	/usr/src/Python-3.6.1/configure --enable-shared && \
-	make install -I /usr/src/Python-3.6.1/
+RUN apt-get update && \
+    apt-get install -y supervisor gcc mysql-client mysql-server default-libmysqlclient-dev
 
 # Variables de entorno
 ENV RPM_PATH=/solutions/app/rpm \
@@ -44,6 +21,6 @@ VOLUME /solutions/app
 
 
 # Configuracion supervisor
-COPY supervisord.conf /etc/supervisord.conf
+COPY supervisord.conf /etc/supervisor/supervisord.conf
 
 CMD ["/usr/bin/supervisord"]
--- initial.py
+++ initial.py
@@ -24,7 +24,7 @@ if bool(RPM_PATH):
 if bool(PIP_PACKAGES):
     for package in PIP_PACKAGES.split(','):
         print('\033[1;32mInstalando paquetes oficiales en PIP: {}\033[0m'.format(package))
-        os.system('pip3.6 install {}'.format(package))
+        os.system('pip install {}'.format(package))
 
 # FALSE if PIP_PATH is '' or None
 if bool(PIP_PATH):
@@ -33,17 +33,17 @@ if bool(PIP_PATH):
     for file in pip_file_list:
         if file.endswith('.zip'):
             print('\033[1;32mInstalando paquetes locales en PIP .zip: {}\033[0m'.format(file))
-            os.system('pip3.6 install {}/{}'.format(PIP_PATH, file))
+            os.system('pip install {}/{}'.format(PIP_PATH, file))
 
         if file.endswith('.tar.gz'):
             print('\033[1;32mInstalando paquetes locales en PIP .tar.gz: {}\033[0m'.format(file))
-            os.system('pip3.6 install {}/{}'.format(PIP_PATH, file))
+            os.system('pip install {}/{}'.format(PIP_PATH, file))
 
 # FALSE if PYTHON_FILES is '' or None
 if bool(PYTHON_FILES):
     for file in PYTHON_FILES.split(','):
         print('\033[1;32mEjecutando ficheros python: {}\033[0m'.format(file))
-        os.system('python3.6 {} &'.format(file))
+        os.system('python {} &'.format(file))
 
 # FALSE if SERVICES is '' or None
 if bool(SERVICES):
